3305c460cf9be811763d9c7ccf95e2abff541e6e
Marcela Mallett <marcelamallett@Marcelas-MacBook-Air.local>
2020-04-21 19:11:28 -0500

    initial commit

---
+++ src/main/java/com/task/todoshare/TodoShareApplication.java
@@ -0,0 +1,13 @@
+package com.task.todoshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoShareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoShareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/task/todoshare/TodoShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.todoshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+//class TodoShareApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
