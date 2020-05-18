8c34b067fb313e158e0be2f4e03550b8e9cba1b9
Will Mallett <mallett002@gmail.com>
2020-05-18 07:06:52 -0500

    making authentication endpoint

---
+++ src/main/java/com/task/todoshare/dto/AuthenticationResponse.java
@@ -0,0 +1,14 @@
+package com.task.todoshare.dto;
+
+public class AuthenticationResponse {
+
+    private final String jwt;
+
+    public AuthenticationResponse(String jwt) {
+        this.jwt = jwt;
+    }
+
+    public String getJwt() {
+        return jwt;
+    }
+}
---
+++ src/main/java/com/task/todoshare/services/JwtUserDetailsService.java
@@ -0,0 +1,21 @@
+package com.task.todoshare.services;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class JwtUserDetailsService implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // get user from database here
+        // repository.getUserFromDB(userName);
+
+        // for this demo, make a fake one
+        return new User("foo", "foo", new ArrayList<>());
+    }
+}
