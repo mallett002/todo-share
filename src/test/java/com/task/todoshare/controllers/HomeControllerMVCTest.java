71dfb1d0c8e1bf2068fb54ca13e05fa0eeef884b
Will Mallett <mallett002@gmail.com>
2020-05-21 06:39:01 -0500

    home controller unit test and AT for todo controller

--- src/test/java/com/task/todoshare/controllers/HomeControllerMVCTest.java
+++ src/test/java/com/task/todoshare/controllers/HomeControllerMVCTest.java
@@ -1,13 +1,12 @@
 package com.task.todoshare.controllers;
 
 import com.task.todoshare.services.GreetingService;
-import org.junit.jupiter.api.Disabled;
+import com.task.todoshare.services.JwtUserDetailsService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-//import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
@@ -24,19 +23,22 @@ public class HomeControllerMVCTest {
     private MockMvc mockMvc;
 
     @MockBean
+    private JwtUserDetailsService jwtUserDetailsService;
+
+    @MockBean
     private GreetingService service;
 
-//    @Disabled
-//    @Test
-//    public void shouldReturnDefaultMessage() throws Exception {
-//        String greeting = "Hello world!";
-//
-//        when(service.greet())
-//            .thenReturn(greeting);
-//
-//        mockMvc.perform(get("/"))
-//            .andDo(print())
-//            .andExpect(status().isOk())
-//            .andExpect(content().string(containsString(greeting)));
-//    }
+    @Test
+    @WithMockUser(username = "user1", password = "pwd", roles = "USER")
+    public void shouldReturnDefaultMessage() throws Exception {
+        String greeting = "Hello world!";
+
+        when(service.greet())
+            .thenReturn(greeting);
+
+        mockMvc.perform(get("/"))
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(content().string(containsString(greeting)));
+    }
 }
